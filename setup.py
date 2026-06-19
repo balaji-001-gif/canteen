6a7b5e6d1f37e0d355ca9891ca30b56274e4aa9c
Balaji K <balajik@Balajis-MacBook-Air.local>
2026-06-19 18:56:24 +0530

    Fix setup.py: read version from file via regex instead of importing package (avoid circular build dep)

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
+import re
 from setuptools import setup, find_packages
 
 with open("requirements.txt") as f:
     install_requires = f.read().strip().split("\n")
 
-from canteen_management import __version__ as version
+with open("canteen_management/__init__.py") as f:
+    match = re.search(r'__version__\s*=\s*["\']([^"\']+)["\']', f.read())
+    if not match:
+        raise RuntimeError("Unable to find version string in canteen_management/__init__.py")
+    version = match.group(1)
 
 setup(
     name="canteen_management",
