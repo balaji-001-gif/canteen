7af5180ff04ffe21703ac38b28b438ecad7689c6
Balaji <balaji@example.com>
2026-06-26 17:50:32 +0530

    feat: auto-fetch wallet balance on POS Invoice when employee is selected
    Adds JS handler to canteen.js that calls get_wallet_balance API when canteen_employee field changes or form is refreshed, populating the canteen_wallet_balance display field.

--- canteen_management/public/js/canteen.js
+++ canteen_management/public/js/canteen.js
@@ -15,3 +15,35 @@ canteen_management.utils = {
         frappe.show_alert({ message: msg, indicator: "red" }, 7);
     },
 };
+
+// POS Invoice: auto-fetch wallet balance when employee is selected
+frappe.ui.form.on("POS Invoice", {
+    refresh: function (frm) {
+        // Populate balance when form loads with an existing employee
+        if (frm.doc.canteen_employee) {
+            canteen_management.utils.update_wallet_balance(frm, frm.doc.canteen_employee);
+        }
+    },
+
+    canteen_employee: function (frm) {
+        canteen_management.utils.update_wallet_balance(frm, frm.doc.canteen_employee);
+    },
+});
+
+canteen_management.utils.update_wallet_balance = function (frm, employee) {
+    if (employee) {
+        frappe.call({
+            method: "canteen_management.api.get_wallet_balance",
+            args: { employee: employee },
+            callback: function (r) {
+                if (r.message && r.message.balance != null) {
+                    frm.set_value("canteen_wallet_balance", r.message.balance);
+                } else {
+                    frm.set_value("canteen_wallet_balance", 0);
+                }
+            },
+        });
+    } else {
+        frm.set_value("canteen_wallet_balance", 0);
+    }
+};
